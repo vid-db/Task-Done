7d31d41dfdfecfab9ddd00ca0636e5fae181cb0a
David <daviddistrajobautista@gmail.com>
2024-06-22 00:13:28 +0800

    set up tailwind, installing lucide-react, and rendering task

---
+++ src/data/todos.ts
@@ -0,0 +1,24 @@
+import { Todo } from "../type/todo";
+
+export const task: Todo[] = [
+    {
+        id: 0,
+        title: "Create Portfolio",
+        completed: false,
+    },
+    {
+        id: 1,
+        title: "Study Next JS",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Make a Responsible UI",
+        completed: true,
+    },
+    {
+        id: 3,
+        title: "Host Project in Heroku",
+        completed: false,
+    },
+];

71dd04597b7ea25ce906915ae0b4940f2653b9ff
David <daviddistrajobautista@gmail.com>
2024-06-24 01:19:48 +0800

    adding style
    revising illustration and color

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ function App() {
 
   return (
    <main className="flex justify-center items-center h-screen w-screen bg-custom-gradient">
-    <div className="lg:w-2/5 sm:w-8/12 xs:w-11/12  bg-white  rounded-xl py-5">
-    <h1 className="font-bold text-3xl text-center ">To-Do List</h1>
+    <div className="lg:w-2/5 sm:w-8/12 xs:w-11/12  bg-white  rounded-xl py-8">
+    <img src="/list to do.png" alt="ToDozImage" className="w-2/5 m-auto" />
+    {/* <h1 className="font-semibold text-3xl text-slate-800 text-center pt-8">To-do List</h1> */}
 
-    <div className="  mx-auto  rounded-sm p-6">
+    <div className="  mx-auto  rounded-sm py-4 px-6">
         <div className="space-y-3">
             {task.map(todo => (
                 <TodoItem todo={todo} />
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -6,7 +6,7 @@ interface TodoItemProps {
 export default function TodoItem ({ todo }: TodoItemProps)  {
   return (
     <div>
-        <label className="flex items-center gap-2 border rounded-md p-3 border-gray-400 bg-white hover:bg-slate-50">
+        <label className="flex items-center gap-2 border rounded-md p-3 border-gray-400 bg-white hover:bg-opacity-50">
             <input type="checkbox" className="scale-125"/>
             <span className={todo.completed ? "line-through text-gray-400" : ""}>
                 {todo.title}

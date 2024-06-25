c536e01ae0dd3598408a04cf203e2fb4e4c74564
David <daviddistrajobautista@gmail.com>
2024-06-26 04:20:29 +0800

    hover effects

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -5,10 +5,10 @@ export default function addToDoForm() {
             <input 
             type="text"
             placeholder="what needs to be done?"
-             className=" grow rounded-3xl border border-gray-400 py-3 px-5 placeholder-gray-400 focus:border-gray-600 outline-none hover:border-gray-600"
+             className=" grow rounded-3xl border border-gray-400 py-3 px-5 placeholder-gray-400 focus:border-gray-600 outline-none hover:border-gray-600 transition transform hover:scale-105"
             />
 
-            <button type="submit" className="rounded-3xl bg-blue-600 p-3 xs:px-4 md:px-6 md:py-3 text-white text-sm md:text-base lg:text-lg"> Add</button>
+            <button type="submit" className="rounded-3xl bg-blue-600 p-3 xs:px-4 md:px-6 md:py-3 text-white text-sm md:text-base lg:text-lg hover:bg-blue-700 transition transform hover:scale-105"> Add</button>
         </form>
     )
 

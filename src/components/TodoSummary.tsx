86a6bc65df106d044a6733e57394b83525688717
David <daviddistrajobautista@gmail.com>
2024-06-30 22:34:16 +0800

    fixing hosting errors

---
+++ src/components/TodoSummary.tsx
@@ -0,0 +1,30 @@
+import { Todo } from "../type/todo";
+
+interface TodoSummaryProps {
+    todos: Todo[];
+    deleteAllCompleted: () => void;
+}
+export default function TodoSummary({
+    todos,
+    deleteAllCompleted
+}: TodoSummaryProps) {
+
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    return (
+        <div className="text-center font">
+            <p className="text-sm font-medium text-gray-500 ">
+                {completedTodos.length}/{todos.length} todos completed
+            </p>
+            {
+                completedTodos.length > 0 && (
+                    <button
+                        onClick={deleteAllCompleted}
+                        className="text-sm font-medium text-red-500 hover:underline">
+                        Delete all completed
+                    </button>
+                )
+            }
+        </div>
+    );
+}

46bab7bc10622ff5474e4a86c806560ea44c259b
David <daviddistrajobautista@gmail.com>
2024-07-14 00:21:32 +0800

    update base repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Simple-To-Do-List/',
+    base: '/Task-Done/',
     plugins: [react()],
 })
 

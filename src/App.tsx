14f1e32df48fd66ba329b3b87aa77ea1f6b7d4d2
David <daviddistrajobautista@gmail.com>
2024-08-25 01:57:40 +0800

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -49,22 +49,21 @@ function App() {
     }
 
     return (
-        <main className="flex justify-center   h-screen w-screen bg-custom-gradient overflow-y-auto">
+        <main className="flex justify-center h-screen w-screen bg-custom-gradient overflow-y-auto">
 
-            <div className="lg:w-4/12 sm:w-3/6 xs:w-11/12 my-auto  bg-slate-50
-      rounded-xl pt-7 pb-6">
+            <div
+                className="lg:w-4/12 sm:w-3/6 xs:w-11/12 my-auto bg-slate-50 rounded-xl pt-7 pb-6">
 
                 <img src="./listTodo.png" alt="ToDosImage" className="w-2/5 m-auto" />
 
-                <div className="  mx-auto  rounded-sm pt-4 pb-8 px-6">
+                <div className="mx-auto rounded-sm pt-4 pb-8 px-6">
 
-                    <AddToDoForm
-                        onSubmit={addTodo}
-                    />
+                    <AddToDoForm onSubmit={addTodo} />
                     <TodoList
                         todos={todos}
                         onCompletedChange={setTodoCompleted}
-                        onDelete={deleteTodo} />
+                        onDelete={deleteTodo}
+                    />
                 </div>
                 <TodoSummary
                     todos={todos}
--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -8,6 +8,7 @@ interface AddTodoFormProps {
 export default function AddToDoForm({ onSubmit }: AddTodoFormProps) {
 
     const [input, setInput] = useState("");
+
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         if (!input.trim()) return;
@@ -25,7 +26,11 @@ export default function AddToDoForm({ onSubmit }: AddTodoFormProps) {
                 className=" grow rounded-3xl border border-gray-400 py-3 px-5   placeholder-gray-400 focus:border-gray-600 outline-none hover:border-gray-600 "
             />
 
-            <button type="submit" className="rounded-3xl bg-blue-600 p-3 xs:px-4 md:px-5 md:py-3 text-white text-sm md:text-base lg:text-lg hover:bg-blue-700 transition transform hover:scale-105"> Add</button>
+            <button
+                type="submit"
+                className="rounded-3xl bg-blue-600 p-3 xs:px-4 md:px-5 md:py-3 text-white text-sm md:text-base lg:text-lg hover:bg-blue-700 transition transform hover:scale-105">
+                Add
+            </button>
         </form>
     );
 
